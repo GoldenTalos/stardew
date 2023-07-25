e867d28c5aa5f9fc26c111510f00566966854143
Sophie <108767907+GoldenTalos@users.noreply.github.com>
2023-07-25 08:31:39 -0400

    vsdz

--- player.py
+++ player.py
@@ -9,11 +9,28 @@ class Player(pygame.sprite.Sprite):
         self.image.fill('green')
         self.rect = self.image.get_rect(center = pos)
 
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2()
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            print('up')
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else: 
+            self.direction.x = 0
+
+        print(self.direction)
+
 
     def update(self, dt):
         self.input()
